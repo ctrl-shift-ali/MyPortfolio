b679dac35004e4a2d81fce48def93ce1fa414164
ctrl-shift-ali <maliabeerkhan1127@gmail.com>
2026-08-10 01:51:10 +0500

    Adding skills

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   base: '/MyPortfolio/',
   plugins: [react()],
   server: {
@@ -11,5 +11,17 @@ export default defineConfig({
   build: {
     target: 'esnext',
     sourcemap: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/index-DD7u5iqJ.js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+            return 'assets/index-CTjmZ65i.css';
+          }
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    },
   },
-});
+}));
